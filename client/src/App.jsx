3f3d7919c9dc362fc4b19737b7e5e1f3b132e852
RahafAj <124215643+RahafAj@users.noreply.github.com>
2025-11-26 19:54:42 +0000

    temp save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,6 @@ const App = () => {
       <Toaster position="top-center" />
 
       <Routes>
-
         {/* Auth Routes */}
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
