01a8e7f31a9866b4524249c7f54841c5183b900b
Rahaf <rahafao@hotmail.com>
2025-11-29 19:06:55 +0300

    fixed reply delete button/reply dates

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -5,11 +5,14 @@ const commentSchema = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
   post: {type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true},
   replies: [
+  new mongoose.Schema(
     {
-      user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-      text: { type: String , required: true}
-    }
-  ]
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      text: { type: String, required: true }
+    },
+    { timestamps: true } //adds createdAt to each reply
+  )
+]
 }, { timestamps: true });
 
 export default mongoose.model("Comments", commentSchema);
